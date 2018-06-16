be1ea68e15d052fd92ad5bcbe4831cdf2c02ec9d
Tom <Tom@qq.com>
2018-06-16 10:38:49 +0800

    第二次修改

--- src/main/java/com/li/maven_test1/App.java
+++ src/main/java/com/li/maven_test1/App.java
@@ -9,5 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println("这是第二次修改");
+        System.out.println("第三次修改");
     }
 }
